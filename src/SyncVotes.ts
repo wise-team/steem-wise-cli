0af219c6b8c3dd5de58dd67e48fa8c15a81e7bf0
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-05-13 11:56:49 +0200

    Update sync-rules code to chainges in core library

--- src/SyncVotes.ts
+++ src/SyncVotes.ts
@@ -3,14 +3,14 @@ import * as program from "commander";
 
 import { objectEquals } from "./util";
 import { ConfigLoader, Config } from "./Config";
-import { SteemSmartvotes, smartvotes_ruleset, smartvotes_operation, smartvotes_voteorder } from "steem-smartvotes";
+import { SteemSmartvotes, smartvotes_ruleset, smartvotes_operation, smartvotes_voteorder, SynchronizationResult } from "steem-smartvotes";
 
 
-export class SyncVotes { // TODO
+export class SyncVotes {
     public static doAction(config: Config): Promise<void> {
         return new Promise((resolve, reject) => {
             const smartvotes = new SteemSmartvotes(config.username, config.postingWif);
-            smartvotes.synchronizeSmartvotes((error: Error | undefined, synced: any [] | undefined): void => {
+            smartvotes.synchronizeSmartvotes((error: Error | undefined, result: SynchronizationResult | undefined): void => {
                 if (error) reject(error);
                 else resolve();
             }, (msg: string, proggress: number): void => {
