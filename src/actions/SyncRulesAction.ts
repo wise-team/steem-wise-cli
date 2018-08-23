c45f61bf3c15f3f747be218e6b9ebfa35d53a82c
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-08-23 13:55:21 +0200

    Load yml rules files

--- src/actions/SyncRulesAction.ts
+++ src/actions/SyncRulesAction.ts
@@ -1,62 +1,64 @@
 import * as fs from "fs";
+import * as _ from "lodash";
 import * as program from "commander";
 
+import { Log } from "../log"; const log = Log.getLogger();
 import { ConfigLoader, Config } from "../config/Config";
 import { Wise, DirectBlockchainApi, SetRules, SetRulesForVoter, EffectuatedSetRules, SteemOperationNumber, SmartvotesOperation } from "steem-wise-core";
 import { Rules } from "../types/Rules";
+import { StaticConfig } from "../config/StaticConfig";
+import { PrioritizedFileObjectLoader } from "../util/PrioritizedFileObjectLoader";
 
 
 export class SyncRulesAction {
     public static doAction(config: Config, rulesIn: string): Promise<string> {
-        return SyncRulesAction.loadJSON(config, rulesIn)
-        .then(SyncRulesAction.syncRules)
+        return SyncRulesAction.loadRules(config, rulesIn)
+        .then((rawRulesets: object []) => SyncRulesAction.syncRules(config, rawRulesets))
         .then((result: SteemOperationNumber | true) => {
             if (result === true) return "Rules are up to date";
             else return "Rules updated: " + result;
         });
     }
 
-    private static loadJSON(config: Config, rulesIn: string): Promise<{config: Config, rawRulesets: object []}> {
-        return new Promise(function(resolve, reject): void {
-            if (!rulesIn || rulesIn.length == 0) throw new Error("You must specify rules to sync (either JSON or path to JSON file)");
+    private static loadRules(config: Config, rulesIn: string): Promise<object []> {
+        const rulesPaths: string [] = _.cloneDeep(StaticConfig.DEFAULT_RULES_FILE_PATHS);
 
-            let jsonStr: string = "";
-
-            const potentialPath: string = rulesIn;
-            if (fs.existsSync(potentialPath)) {
-                jsonStr = fs.readFileSync(potentialPath).toString();
-            }
-            else {
-                jsonStr = rulesIn;
-            }
-
-            let data: object [] | undefined = undefined;
+        if (rulesIn && rulesIn.length > 0) {
             try {
-                const input: object = JSON.parse(jsonStr);
+                let data: object [] | undefined = undefined;
+                const input: object = JSON.parse(rulesIn);
                 if (!(Object.prototype.toString.call(input) === "[object Array]")) throw new Error("Rules should be an array");
                 data = input  as object [];
+
+                return Promise.resolve(data); // succes parsing inline json
             }
-            catch (e) {
-                reject(e);
+            catch (error) {
+                log.debug("Failed to parse " + rulesIn + " as inline JSON. Proceeding to loading files. Adding " + rulesIn + " as top-priority file.");
+                rulesPaths.unshift(rulesIn);
             }
-            if (data) resolve({config: config, rawRulesets: data});
-            else throw new Error("Could not load rulesets from file/json");
+        }
+
+        return PrioritizedFileObjectLoader.loadFromFilesNoMerge([], rulesPaths, "rules")
+        .then((result: object [] | undefined) => {
+            if (!result) throw new Error("Could not load rulesets from any of the files: " + _.join(rulesPaths, ", "));
+            if (!(Object.prototype.toString.call(result) === "[object Array]")) throw new Error("Rules should be an array");
+            return result;
         });
     }
 
-    private static syncRules(input: {config: Config, rawRulesets: object []}): Promise<SteemOperationNumber | true> {
+    private static syncRules(config: Config, rawRulesets: object []): Promise<SteemOperationNumber | true> {
         return Promise.resolve()
         .then(() => {
-            const newRules: SetRulesForVoter [] = input.rawRulesets as SetRulesForVoter [];
+            const newRules: SetRulesForVoter [] = rawRulesets as SetRulesForVoter [];
 
             newRules.forEach((element: SetRulesForVoter) => {
                 if (!element.voter) return Promise.reject(new Error("Voter should be specified for each SetRules"));
                 if (!element.rulesets) return Promise.reject(new Error("Rulesets should be specified for each voter"));
             });
 
-            const api: DirectBlockchainApi = new DirectBlockchainApi(input.config.username, input.config.postingWif);
-            if (input.config.disableSend) api.setSendEnabled(false);
-            const delegatorWise = new Wise(input.config.username, api);
+            const api: DirectBlockchainApi = new DirectBlockchainApi(config.username, config.postingWif);
+            if (config.disableSend) api.setSendEnabled(false);
+            const delegatorWise = new Wise(config.username, api);
 
             return delegatorWise.diffAndUpdateRulesAsync(newRules, (msg: string, proggress: number) => {
                 console.log("[syncing a rule][" + Math.floor(proggress * 100) + "%]: " + msg);
--- src/util/PrioritizedFileObjectLoader.ts
+++ src/util/PrioritizedFileObjectLoader.ts
@@ -6,6 +6,18 @@ import { Log } from "../log"; const log = Log.getLogger();
 
 export class PrioritizedFileObjectLoader {
     public static loadFromFiles<T>(defaultObject: T, filePaths: string [], descriptionForWarnings: string): Promise<T> {
+        return PrioritizedFileObjectLoader._loadFromFiles(defaultObject, filePaths, descriptionForWarnings, true)
+        .then((result: T | undefined) => {
+            if (!result) throw new Error("PrioritizedFileObjectLoader.loadFromFiles: result is null. Maybe the default object was null?");
+            return result;
+        });
+    }
+
+    public static loadFromFilesNoMerge<T>(defaultObject: T, filePaths: string [], descriptionForWarnings: string): Promise<T | undefined> {
+        return PrioritizedFileObjectLoader._loadFromFiles(defaultObject, filePaths, descriptionForWarnings, false);
+    }
+
+    private static _loadFromFiles<T>(defaultObject: T, filePaths: string [], descriptionForWarnings: string, merge: boolean = true): Promise<T | undefined> {
         return Promise.resolve().then(() => {
             for (let i = 0; i < filePaths.length; i++) {
                 const filePath = filePaths[i];
@@ -23,26 +35,27 @@ export class PrioritizedFileObjectLoader {
                     // if continue was not called, try to load as JSON:
                     try {
                         const loadedObj = JSON.parse(fileContents) as T;
-                        const mergedObj = _.merge({}, defaultObject, loadedObj);
-                        return mergedObj;
+                        if (merge) return _.merge({}, defaultObject, loadedObj);
+                        else return loadedObj;
                     }
                     catch (error) {
-                        log.debug("Failed to parse " + descriptionForWarnings + " (" + filePath + ") as JSON " + error.message);
+                        log.debug("Failed to parse " + descriptionForWarnings + " (" + filePath + ") as JSON: " + error.message);
                     }
                     // try to load as YAML:
                     try {
                         const loadedObj = yaml.safeLoad(fileContents) as T;
-                        const mergedObj = _.merge({}, defaultObject, loadedObj);
-                        return mergedObj;
+                        if (merge) return _.merge({}, defaultObject, loadedObj);
+                        else return loadedObj;
                     }
                     catch (error) {
-                        log.debug("Failed to parse " + descriptionForWarnings + " (" + filePath + ") as YAML " + error.message);
+                        log.debug("Failed to parse " + descriptionForWarnings + " (" + filePath + ") as YAML: " + error.message);
                         throw new Error("The " + descriptionForWarnings + " file " + filePath + " is malformed. Failed to parse it as JSON or YAML.");
                     }
                 }
             }
             log.warn("No " + descriptionForWarnings + " was loaded");
-            return defaultObject;
+            if (merge) return defaultObject;
+            else return undefined;
         });
     }
 }
