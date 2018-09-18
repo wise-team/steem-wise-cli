9a524b0430773893f5459b1fb7b1c1dcd6d96c5d
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-09-18 18:04:07 +0200

    build(release script): create release script
    Release script similar to the one in steem-wise-core

--- scripts/release.sh
+++ scripts/release.sh
@@ -17,11 +17,22 @@ if [ "$(git rev-parse --abbrev-ref HEAD)" != "${REQUIRED_BRANCH}" ]; then
     exit 1
 fi
 
+if [[ "$(node --version)" = "$(cat .nvmrc)"* ]]; then
+    echo "Node version correct ($(node --version))"
+else 
+    echo "Error: Node version does not match .nvmrc"
+    exit 1
+fi
 
-echo "Updating steem-wise-cli to ${VERSION}"
+if [[ -z "${CONVENTIONAL_GITHUB_RELEASER_TOKEN}" ]]; then
+    echo "You need to set up CONVENTIONAL_GITHUB_RELEASER_TOKEN env. It should contain github token with scope \"public_repo\"."
+    exit 1;
+fi
+
+
+echo "Updating version of steem-wise-core in package.json to ${VERSION}"
 node -e " \
 var packageFileContents = require(\"./package.json\"); \
-packageFileContents.version = \"${VERSION}\"; \
 packageFileContents.dependencies[\"steem-wise-core\"] = \"^${VERSION}\"; \
 require('fs').writeFileSync(\"./package.json\", JSON.stringify(packageFileContents, null, 2), \"utf8\"); \
 "
@@ -34,15 +45,37 @@ npm run build
 echo "Build successful"
 
 
+echo "Updating steem-wise-cli to ${VERSION}"
+node -e " \
+var packageFileContents = require(\"./package.json\"); \
+packageFileContents.version = \"${VERSION}\"; \
+require('fs').writeFileSync(\"./package.json\", JSON.stringify(packageFileContents, null, 2), \"utf8\"); \
+"
+echo "Updating version succeeded"
+
+
+echo "Generating changelog"
+npm run changelog
+echo "Generating changelog correct"
+
+
 echo "Creating git tag"
-git add package.json package-lock.json
+git add package.json package-lock.json CHANGELOG.md
 git commit -m "Semver ${VERSION}"
 git push
 git tag -a "v${VERSION}" -m "Steem WISE command line tool version ${VERSION}"
 git push --tags
 echo "Done creating tag"
 
+
 echo "Publishing to npmjs.com registry"
 npm publish
+echo "Done publishing"
+
+
+echo "Creating github release"
+conventional-github-releaser -p angular
+echo "Github release done"
+
 
 echo "Done"
